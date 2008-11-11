86ed564de5e9797a90a3691ca348873ecd463d7b
San Mehat <san@google.com>
2008-11-10 16:29:50 -0800

    mmc: Add status IRQ and status callback function to mmc platform data
    Signed-off-by: San Mehat <san@google.com>

--- include/linux/amba/mmci.h
+++ include/linux/amba/mmci.h
@@ -38,6 +38,8 @@ struct mmci_platform_data {
 	int	gpio_cd;
 	bool	cd_invert;
 	unsigned long capabilities;
+	unsigned int status_irq;
+	int (*register_status_notify)(void (*callback)(int card_present, void *dev_id), void *dev_id);
 };
 
 #endif
