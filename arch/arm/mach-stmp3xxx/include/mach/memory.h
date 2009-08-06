82e8f4a0d1de72feb4d12b79fbdca7695da6302e
Xinyu Chen <Xinyu.Chen@freescale.com>
2009-08-06 17:34:24 +0800

    ENGR00114922 MX233 Bring DMA ZONE to kernel
    Split memory zone to DMA and normal zone for dma allocation.
    Make the DMA zone size configurable.
    
    Signed-off-by: Xinyu Chen <xinyu.chen@freescale.com>

--- arch/arm/mach-stmp3xxx/include/mach/memory.h
+++ arch/arm/mach-stmp3xxx/include/mach/memory.h
@@ -14,11 +14,39 @@
 #ifndef __ASM_ARCH_MEMORY_H
 #define __ASM_ARCH_MEMORY_H
 
+#include <asm/page.h>
+#include <asm/sizes.h>
+
 /*
  * Physical DRAM offset.
  */
 #define PHYS_OFFSET	UL(0x40000000)
 #define BUS_OFFSET	UL(0x40000000)
+
+#ifndef __ASSEMBLY__
+
+#ifdef CONFIG_DMA_ZONE_SIZE
+#define MXC_DMA_ZONE_SIZE	((CONFIG_DMA_ZONE_SIZE * SZ_1M) >> PAGE_SHIFT)
+#else
+#define MXC_DMA_ZONE_SIZE	((12 * SZ_1M) >> PAGE_SHIFT)
+#endif
+
+static inline void __arch_adjust_zones(int node, unsigned long *zone_size,
+				       unsigned long *zhole_size)
+{
+	if (node != 0)
+		return;
+	/* Create separate zone to reserve memory for DMA */
+	zone_size[1] = zone_size[0] - MXC_DMA_ZONE_SIZE;
+	zone_size[0] = MXC_DMA_ZONE_SIZE;
+	zhole_size[1] = zhole_size[0];
+	zhole_size[0] = 0;
+}
+
+#define arch_adjust_zones(node, size, holes) \
+	__arch_adjust_zones(node, size, holes)
+
+#endif
 
 /*
  * Virtual view <-> DMA view memory address translations
@@ -32,6 +60,6 @@
 
 #define ISA_DMA_THRESHOLD	(0x0003ffffULL)
 
-#define CONSISTENT_DMA_SIZE	SZ_4M
+#define CONSISTENT_DMA_SIZE	SZ_32M
 
 #endif
