b5e3c818291ee66b4d5403d00eb75ff8a1254e72
Raj Rajasekaran <b10872@freescale.com>
2009-08-19 17:28:32 -0500

    ENGR00112764 MX23: DCP crytpo test DCP-AES fail. 100%.
    Changed AES config option to be built-into the kernel.
    
    stmp3xxx_dcp_probe() - Moved Algorithm registration below
    platform driver data setting and interrupt request calls.
    So that algorithms are registered after the platform data is
    set. This is done to make it work with latest linux crypto
    API changes.
    
    Signed-off-by: Raj Rajasekaran <b10872@freescale.com>

--- drivers/crypto/stmp3xxx_dcp.c
+++ drivers/crypto/stmp3xxx_dcp.c
@@ -1225,32 +1225,6 @@ static int stmp3xxx_dcp_probe(struct platform_device *pdev)
 		init_completion(&sdcp->op_wait[i]);
 	}
 
-	ret = crypto_register_alg(&dcp_aes_alg);
-	if (ret != 0)  {
-		dev_err(&pdev->dev, "Failed to register aes crypto\n");
-		goto err_kfree;
-	}
-
-	ret = crypto_register_alg(&dcp_aes_ecb_alg);
-	if (ret != 0)  {
-		dev_err(&pdev->dev, "Failed to register aes ecb crypto\n");
-		goto err_unregister_aes;
-	}
-
-	ret = crypto_register_alg(&dcp_aes_cbc_alg);
-	if (ret != 0)  {
-		dev_err(&pdev->dev, "Failed to register aes cbc crypto\n");
-		goto err_unregister_aes_ecb;
-	}
-
-#ifndef DISABLE_SHA1
-	ret = crypto_register_alg(&dcp_sha1_alg);
-	if (ret != 0)  {
-		dev_err(&pdev->dev, "Failed to register aes cbc crypto\n");
-		goto err_unregister_aes_cbc;
-	}
-#endif
-
 	platform_set_drvdata(pdev, sdcp);
 
 	/* Soft reset and remove the clock gate */
@@ -1309,6 +1283,32 @@ static int stmp3xxx_dcp_probe(struct platform_device *pdev)
 	}
 
 	global_sdcp = sdcp;
+
+	ret = crypto_register_alg(&dcp_aes_alg);
+	if (ret != 0)  {
+		dev_err(&pdev->dev, "Failed to register aes crypto\n");
+		goto err_kfree;
+	}
+
+	ret = crypto_register_alg(&dcp_aes_ecb_alg);
+	if (ret != 0)  {
+		dev_err(&pdev->dev, "Failed to register aes ecb crypto\n");
+		goto err_unregister_aes;
+	}
+
+	ret = crypto_register_alg(&dcp_aes_cbc_alg);
+	if (ret != 0)  {
+		dev_err(&pdev->dev, "Failed to register aes cbc crypto\n");
+		goto err_unregister_aes_ecb;
+	}
+
+#ifndef DISABLE_SHA1
+	ret = crypto_register_alg(&dcp_sha1_alg);
+	if (ret != 0)  {
+		dev_err(&pdev->dev, "Failed to register aes cbc crypto\n");
+		goto err_unregister_aes_cbc;
+	}
+#endif
 
 	dev_notice(&pdev->dev, "DCP crypto enabled.!\n");
 	return 0;
