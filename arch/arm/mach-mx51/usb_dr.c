8635334b08952f242aba2e7cf916a47b117f4169
Albert Chen <R65187@freescale.com>
2009-10-09 16:44:20 +0800

    ENGR00115875  add USB gadget low power mode feature.
    	USB will enter low power mode if no vbus power supply.
    	Only finished this feature in Mx37.
    	1. When enter low power mode, driver will close usb related clocks
    		as possible as could.
    		set usb IP to stop.
    		Set PHY to low power suspend by setting PHCD bit of portsc.
    	2. This patch doesn't support low power mode in OTG mode yet.
    
    Signed-off-by: Albert Chen <r65187@freescale.com>

--- arch/arm/mach-mx51/usb_dr.c
+++ arch/arm/mach-mx51/usb_dr.c
@@ -21,6 +21,7 @@
 
 static int usbotg_init_ext(struct platform_device *pdev);
 static void usbotg_uninit_ext(struct fsl_usb2_platform_data *pdata);
+static void _wake_up_enable(struct fsl_usb2_platform_data *pdata, bool enable);
 
 /*
  * platform data structs
@@ -35,6 +36,7 @@ static struct fsl_usb2_platform_data __maybe_unused dr_utmi_config = {
 	.power_budget      = 500,		/* 500 mA max power */
 	.gpio_usb_active   = gpio_usbotg_hs_active,
 	.gpio_usb_inactive = gpio_usbotg_hs_inactive,
+	.wake_up_enable = _wake_up_enable,
 	.transceiver       = "utmi",
 };
 
@@ -127,6 +129,21 @@ static void usbotg_uninit_ext(struct fsl_usb2_platform_data *pdata)
 	clk_put(usb_clk);
 
 	usbotg_uninit(pdata);
+}
+
+static void _wake_up_enable(struct fsl_usb2_platform_data *pdata, bool enable)
+{
+	if (get_usb_mode(pdata) == FSL_USB_DR_DEVICE) {
+		if (enable) {
+			USBCTRL |= UCTRL_OWIE;
+			USBCTRL_HOST2 |= UCTRL_H2OVBWK_EN;
+			USB_PHY_CTR_FUNC |= USB_UTMI_PHYCTRL_CONF2;
+		} else {
+			USBCTRL &= ~UCTRL_OWIE;
+			USBCTRL_HOST2 &= ~UCTRL_H2OVBWK_EN;
+			USB_PHY_CTR_FUNC &= ~USB_UTMI_PHYCTRL_CONF2;
+		}
+	}
 }
 
 static int __init usb_dr_init(void)
--- arch/arm/plat-mxc/include/mach/arc_otg.h
+++ arch/arm/plat-mxc/include/mach/arc_otg.h
@@ -261,6 +261,8 @@
 #define UCTRL_H2PP		(1 << 18)	/* Power Polarity for uh2 */
 #define UCTRL_H2PM		(1 << 16)	/* HOST2 power mask */
 #endif
+#define UCTRL_H2OVBWK_EN	(1 << 6) /* OTG VBUS Wakeup Enable */
+#define UCTRL_H2OIDWK_EN	(1 << 5) /* OTG ID Wakeup Enable */
 
 #define UCTRL_H1WIR		(1 << 15)	/* HOST1 wakeup intr request received */
 #define UCTRL_H1SIC_MASK	(3 << 13)	/* HOST1 Serial Interface Config: */
@@ -271,6 +273,13 @@
 #define UCTRL_OLOCKD		(1 << 13)	/* otg lock disable */
 #define UCTRL_H2LOCKD		(1 << 12)	/* HOST2 lock disable */
 #define UCTRL_H1UIE		(1 << 12)	/* Host1 ULPI interrupt enable */
+
+#if defined(CONFIG_ARCH_MX37)
+/* VBUS wakeup enable, UTMI only */
+#define UCTRL_VBUS_WKUP_EN	(1 << 12)
+#elif defined(CONFIG_ARCH_MX25) || defined(CONFIG_ARCH_MX35)
+#define UCTRL_VBUS_WKUP_EN      (1 << 15)
+#endif
 
 #define UCTRL_PP                (1 << 11)       /* power polarity bit */
 #define UCTRL_H1WIE		(1 << 11)	/* HOST1 wakeup intr enable */
@@ -309,6 +318,9 @@
 						/* 0=low : Operate as A-device */
 
 /* USB_PHY_CTRL_FUNC */
+/* PHY control0 Register Bit Masks */
+#define USB_UTMI_PHYCTRL_CONF2	(1 << 26)
+
 #define USB_UTMI_PHYCTRL_UTMI_ENABLE (1 << 24)
 #define USB_UTMI_PHYCTRL_CHGRDETEN (1 << 24)    /* Enable Charger Detector */
 #define USB_UTMI_PHYCTRL_CHGRDETON (1 << 23)    /* Charger Detector Power On Control */
@@ -340,4 +352,6 @@
 #define ULPIVW_WDATA_SHIFT	0
 
 #define HCSPARAMS_PPC           (0x1<<4)        /* Port Power Control */
+
+extern enum fsl_usb2_modes get_usb_mode(struct fsl_usb2_platform_data *pdata);
 #endif
