23117a1fcf4366f6d6f1ef733b6050a61ac999f2
Lily Zhang <r58066@freescale.com>
2010-03-20 15:51:55 +0800

    ENGR00121658 MX53 Disable ARM SRPG
    Currently the system crashes if ARM SRPG is enabled when ARM executes WFI.
    Disable it for MX53 until a fix is identified.
    
    Signed-off-by: Ranjani Vaidyanathan-RA5478 <Ranjani.Vaidyanathan@freescale.com>
    Signed-off-by: Lily Zhang <r58066@freescale.com>

--- arch/arm/mach-mx5/system.c
+++ arch/arm/mach-mx5/system.c
@@ -94,7 +94,8 @@ void mxc_cpu_lp_set(enum mxc_cpu_pwr_mode mode)
 
 	__raw_writel(plat_lpc, MXC_CORTEXA8_PLAT_LPC);
 	__raw_writel(ccm_clpcr, MXC_CCM_CLPCR);
-	__raw_writel(arm_srpgcr, MXC_SRPG_ARM_SRPGCR);
+	if (cpu_is_mx51())
+		__raw_writel(arm_srpgcr, MXC_SRPG_ARM_SRPGCR);
 	__raw_writel(arm_srpgcr, MXC_SRPG_NEON_SRPGCR);
 	if (stop_mode) {
 		__raw_writel(empgc0, MXC_SRPG_EMPGC0_SRPGCR);
