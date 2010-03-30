eeb3bb9186ef5e16a1ebe92c7ffa6d46476ccf6d
Xinyu Chen <xinyu.chen@freescale.com>
2010-03-30 15:46:10 +0800

    ENGR00122137 MX53 Add UART support for EVK APC
    Add UART2/3 support for EVK Automotive Port Card.
    
    Signed-off-by: Xinyu Chen <xinyu.chen@freescale.com>

--- arch/arm/mach-mx5/board-mx53_evk.h
+++ arch/arm/mach-mx5/board-mx53_evk.h
@@ -75,10 +75,10 @@
 /*! @} */
 /* UART 2 configuration */
 #define UART2_MODE		MODE_DCE
-#define UART2_IR		IRDA
+#define UART2_IR		NO_IRDA
 #define UART2_ENABLED		1
 /* UART 3 configuration */
-#define UART3_MODE		MODE_DTE
+#define UART3_MODE		MODE_DCE
 #define UART3_IR		NO_IRDA
 #define UART3_ENABLED		1
 /* UART 4 configuration */
