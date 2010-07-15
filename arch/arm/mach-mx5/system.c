6dd9d089492e0bbbcbe163c77cc5ae0c9b2c6d6b
Richard Zhao <richard.zhao@freescale.com>
2010-07-15 10:57:14 +0800

    ENGR00125169 MX5: only reset nfc in arch_reset when we have it
    it fix mx50 reboot wdog reg write failed issue.
    
    Signed-off-by: Richard Zhao <richard.zhao@freescale.com>

--- arch/arm/mach-mx5/system.c
+++ arch/arm/mach-mx5/system.c
@@ -171,7 +171,8 @@ void arch_reset(char mode)
 	/* Workaround to reset NFC_CONFIG3 register
 	 * due to the chip warm reset does not reset it
 	 */
-	__raw_writel(0x20600, IO_ADDRESS(NFC_BASE_ADDR) + 0x28);
+	if (cpu_is_mx51() || cpu_is_mx53())
+		__raw_writel(0x20600, IO_ADDRESS(NFC_BASE_ADDR) + 0x28);
 
 	/* Assert SRS signal */
 	mxc_wd_reset();
