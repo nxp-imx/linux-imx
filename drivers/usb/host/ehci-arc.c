7b7511158fc067acede92d50c679aedcb22c9c66
Peter Chen <peter.chen@freescale.com>
2011-03-24 14:25:45 +0800

    ENGR00141092 usb: fix some tiny bugs for usb
    For android system, the gadget driver is built in, when the usb
    device is on otg port, the udc suspended counter will be error at
    the situation. This patch fix it, if this situation is met, the
    udc suspended counter will be not added.
    
    Using A VBus Valid to judge whether the usb cable is connected, as
    some boards' vbus may higher than 0.8v(B Session Valid)
    
    Signed-off-by: Peter Chen <peter.chen@freescale.com>

--- drivers/usb/host/ehci-arc.c
+++ drivers/usb/host/ehci-arc.c
@@ -33,6 +33,8 @@ extern int usb_host_wakeup_irq(struct device *wkup_dev);
 extern void usb_host_set_wakeup(struct device *wkup_dev, bool para);
 static void fsl_usb_lowpower_mode(struct fsl_usb2_platform_data *pdata, bool enable)
 {
+	pdata->lowpower = enable;
+
 	if (enable) {
 		if (pdata->phy_lowpower_suspend)
 			pdata->phy_lowpower_suspend(pdata, true);
@@ -40,7 +42,6 @@ static void fsl_usb_lowpower_mode(struct fsl_usb2_platform_data *pdata, bool ena
 		if (pdata->phy_lowpower_suspend)
 			pdata->phy_lowpower_suspend(pdata, false);
 	}
-	pdata->lowpower = enable;
 }
 
 static void fsl_usb_clk_gate(struct fsl_usb2_platform_data *pdata, bool enable)
