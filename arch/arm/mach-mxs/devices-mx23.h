b92b6f505cb2f7d3409783585759c699a478eed5
Huang Shijie <b32955@freescale.com>
2011-04-15 17:19:26 +0800

    ENGR00141558-2 ARM: mxs: add GPMI device for imx23
    add the gpmi device for imx23.
    
    Signed-off-by: Huang Shijie <b32955@freescale.com>

--- arch/arm/mach-mxs/devices-mx23.h
+++ arch/arm/mach-mxs/devices-mx23.h
@@ -2,7 +2,7 @@
  * Copyright (C) 2010 Pengutronix
  * Uwe Kleine-Koenig <u.kleine-koenig@pengutronix.de>
  *
- * Copyright 2010 Freescale Semiconductor, Inc. All Rights Reserved.
+ * Copyright 2010-2011 Freescale Semiconductor, Inc.
  *
  * This program is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License version 2 as published by the
@@ -20,6 +20,10 @@ extern const struct mxs_auart_data mx23_auart_data[] __initconst;
 #define mx23_add_auart(id)	mxs_add_auart(&mx23_auart_data[id])
 #define mx23_add_auart0()		mx23_add_auart(0)
 #define mx23_add_auart1()		mx23_add_auart(1)
+
+extern const struct mxs_gpmi_nfc_data mx23_gpmi_nfc_data __initconst;
+#define mx23_add_gpmi_nfc(pdata)	\
+	mxs_add_gpmi_nfc(pdata, &mx23_gpmi_nfc_data)
 
 #define mx23_add_mxs_pwm(id)		mxs_add_mxs_pwm(MX23_PWM_BASE_ADDR, id)
 
