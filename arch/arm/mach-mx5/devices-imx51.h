7bfea6fca7d1ee9328aa6788b57634f50c8e55a3
Anish Trivedi <anish@freescale.com>
2011-04-18 13:59:27 -0500

    ENGR00142296-1 ARM: MX5: Add SRTC device support
    Machine layer changes to add SRTC device for MX5x platforms.
    
    Signed-off-by: Anish Trivedi <anish@freescale.com>

--- arch/arm/mach-mx5/devices-imx51.h
+++ arch/arm/mach-mx5/devices-imx51.h
@@ -15,6 +15,10 @@ extern const struct imx_fec_data imx51_fec_data __initconst;
 
 #define imx51_add_gpio_keys(pdata) imx_add_gpio_keys(pdata)
 
+extern const struct imx_srtc_data imx51_imx_srtc_data __initconst;
+#define imx51_add_srtc()	\
+	imx_add_srtc(&imx51_imx_srtc_data)
+
 extern const struct imx_imx_i2c_data imx51_imx_i2c_data[] __initconst;
 #define imx51_add_imx_i2c(id, pdata)	\
 	imx_add_imx_i2c(&imx51_imx_i2c_data[id], pdata)
