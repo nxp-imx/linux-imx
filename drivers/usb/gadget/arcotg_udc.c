ba9370c4a165543a6da10de5368884a0dad49852
Peter Chen <peter.chen@freescale.com>
2011-11-23 14:17:11 +0800

    ENGR00162947 arcotg: Keep usb active when user calls fsl_pullup
    Android gadget driver will request pullup when enable
    android gadget after add all functions' config.
    Otherwise the fsl_pullup causes kernel hang without usb_oh3_clock on.
    
    Signed-off-by: Peter Chen <peter.chen@freescale.com>

--- drivers/usb/gadget/arcotg_udc.c
+++ drivers/usb/gadget/arcotg_udc.c
@@ -1430,12 +1430,18 @@ static int fsl_pullup(struct usb_gadget *gadget, int is_on)
 
 	udc = container_of(gadget, struct fsl_udc, gadget);
 	udc->softconnect = (is_on != 0);
-	if (can_pullup(udc))
+	dr_clk_gate(true);
+	if (can_pullup(udc)) {
 		fsl_writel((fsl_readl(&dr_regs->usbcmd) | USB_CMD_RUN_STOP),
 				&dr_regs->usbcmd);
-	else
+		dr_phy_low_power_mode(udc, false);
+	} else {
 		fsl_writel((fsl_readl(&dr_regs->usbcmd) & ~USB_CMD_RUN_STOP),
 				&dr_regs->usbcmd);
+		dr_wake_up_enable(udc, true);
+		dr_phy_low_power_mode(udc, true);
+	}
+	dr_clk_gate(false);
 
 	return 0;
 }
