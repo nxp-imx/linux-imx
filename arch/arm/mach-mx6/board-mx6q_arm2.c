3703b55d2c5dbbef8df20b78c85be721effaf993
Liu Xiaowen <b37945@freescale.com>
2011-11-25 10:16:12 +0800

    ENGR00163108 remove pmem cache property for arm2.
    remove pmem cache property for arm2.
    
    Signed-off-by: Liu Xiaowen <b37945@freescale.com>

--- arch/arm/mach-mx6/board-mx6q_arm2.c
+++ arch/arm/mach-mx6/board-mx6q_arm2.c
@@ -925,7 +925,6 @@ static struct android_pmem_platform_data android_pmem_data = {
 static struct android_pmem_platform_data android_pmem_gpu_data = {
        .name = "pmem_gpu",
        .size = SZ_128M,
-       .cached = 1,
 };
 
 static struct fsl_mxc_hdmi_platform_data hdmi_data = {
