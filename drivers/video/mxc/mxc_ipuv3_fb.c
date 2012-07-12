4058e6dec3232dbf395bb4cab617c70ce30c767b
Liu Ying <Ying.liu@freescale.com>
2012-07-12 17:14:42 +0800

    ENGR00216989-2 IPUv3 FB:Cache u/v offset in suspend for crop case
    V4l2 output renders video on a framebuffer. To get better performance,
    VPU decoded YUV frames may be used by IPU display channel and bypass
    IPU IC channel processing. 1080P video playing on 1080P display
    matches this case. As 1080P video's resolution is usually 1920x1088,
    so we use IPU display channel to do input cropping to 1920x1080 to
    align with display resolution, which involves IPU display channel
    u/v offset setting if video frame's is in interleaved or patial-
    interleaved YUV pixel format. Currently, ipuv3 fb driver doesn't support
    this case, u/v offset setting is done in v4l2 output driver. Android
    ipuv3 fb driver late resume function may be called after v4l2 output
    sets u/v offset setting and overwrite the setting to zero, which makes
    the video shows wrongly after suspend/resume. This patch caches u/v
    offset setting in ipuv3 fb suspend function and sets it back in resume
    function to fix the issue.
    
    Signed-off-by: Liu Ying <Ying.Liu@freescale.com>
    (cherry picked from commit 43e3f7d277f8afb09610d9638552a52e47c9ad71)

--- drivers/video/mxc/mxc_ipuv3_fb.c
+++ drivers/video/mxc/mxc_ipuv3_fb.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2004-2011 Freescale Semiconductor, Inc. All Rights Reserved.
+ * Copyright 2004-2012 Freescale Semiconductor, Inc. All Rights Reserved.
  */
 
 /*
@@ -87,6 +87,16 @@ struct mxcfb_info {
 	struct completion vsync_complete;
 
 	bool fb_suspended;
+
+	/*
+	 * Cached u/v offset for suspend/resume.
+	 * Only used to sync with v4l output for
+	 * interleaved or partial interleaved
+	 * YUV pixel format in case cropping is
+	 * needed.
+	 */
+	uint32_t uoffset;
+	uint32_t voffset;
 };
 
 struct mxcfb_mode {
@@ -326,7 +336,8 @@ static int _setup_disp_channel2(struct fb_info *fbi)
 					 base,
 					 (fbi->var.accel_flags ==
 					  FB_ACCEL_TRIPLE_FLAG) ? base : 0,
-					 0, 0);
+					 mxc_fbi->uoffset,
+					 mxc_fbi->voffset);
 	if (retval) {
 		dev_err(fbi->device,
 			"ipu_init_channel_buffer error %d\n", retval);
@@ -1489,6 +1500,8 @@ static int mxcfb_suspend(struct platform_device *pdev, pm_message_t state)
 	fb_set_suspend(fbi, 1);
 	saved_blank = mxc_fbi->cur_blank;
 	mxcfb_blank(FB_BLANK_POWERDOWN, fbi);
+	ipu_get_channel_uvoffset(mxc_fbi->ipu_ch, IPU_INPUT_BUFFER,
+				 &mxc_fbi->uoffset, &mxc_fbi->voffset);
 	mxc_fbi->next_blank = saved_blank;
 	mxc_fbi->fb_suspended = true;
 	release_console_sem();
@@ -1507,6 +1520,7 @@ static int mxcfb_resume(struct platform_device *pdev)
 	acquire_console_sem();
 	mxc_fbi->fb_suspended = false;
 	mxcfb_blank(mxc_fbi->next_blank, fbi);
+	mxc_fbi->uoffset = mxc_fbi->voffset = 0;
 	fb_set_suspend(fbi, 0);
 	release_console_sem();
 
@@ -1871,6 +1885,7 @@ static int mxcfb_probe(struct platform_device *pdev)
 	mxcfbi->ipu_di = pdev->id;
 	mxcfbi->ipu_alp_ch_irq = -1;
 	mxcfbi->fb_suspended = false;
+	mxcfbi->uoffset = mxcfbi->voffset = 0;
 
 	if (pdev->id == 0) {
 		ipu_disp_set_global_alpha(mxcfbi->ipu_ch, true, 0x80);
