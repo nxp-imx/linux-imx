bc39e797d35852114be69269bbdbae3b849e2d57
Li Jun <jun.li@nxp.com>
2017-01-11 18:11:57 +0800

    MLK-13721 usb: chipidea: otg: add timeout for wait usb device disconnect
    While quickly switch the OTG port between host and gadget by ID cable plug
    and unplug with a usb net device, the disconnect event may be lost, but
    we have a wait-for-ever on usb device disconnection before stop host role
    when switch from host to gadget, so this wait will not finish and the irq
    not be enabled in this case, in the end this port can't be used at all.
    
    This patch adds a wait timeout(6s~9s) to let a normal disconnect event
    finished by host driver, if timeout, we just start to stop host anyway.
    
    Acked-by: Peter Chen <peter.chen@nxp.com>
    Signed-off-by: Li Jun <jun.li@nxp.com>

--- drivers/usb/chipidea/otg.c
+++ drivers/usb/chipidea/otg.c
@@ -2,6 +2,7 @@
  * otg.c - ChipIdea USB IP core OTG driver
  *
  * Copyright (C) 2013-2016 Freescale Semiconductor, Inc.
+ * Copyright 2017 NXP
  *
  * Author: Peter Chen
  *
@@ -112,13 +113,14 @@ void ci_handle_vbus_change(struct ci_hdrc *ci)
 void ci_handle_id_switch(struct ci_hdrc *ci)
 {
 	enum ci_role role = ci_otg_role(ci);
-	int ret = 0;
+	int ret = 0, wait_count = 0;
 
 	if (role != ci->role) {
 		dev_dbg(ci->dev, "switching from %s to %s\n",
 			ci_role(ci)->name, ci->roles[role]->name);
 
-		while (ci_hdrc_host_has_device(ci)) {
+		/* Wait 6s ~ 9s for disconncet finish */
+		while (ci_hdrc_host_has_device(ci) && wait_count++ < 600) {
 			enable_irq(ci->irq);
 			usleep_range(10000, 15000);
 			disable_irq_nosync(ci->irq);
