d5c728fba8bdb74c9dc759ae38918cb303a42bc4
Leonard Crestez <leonard.crestez@nxp.com>
2017-01-26 13:39:02 +0200

    MLK-13798 tty: serial: imx: Only poke at suspend wakeup bits in noirq phase
    This prevents unwanted interrupts coming in early suspend.
    
    Loosely based on 81e8e7d91d81351d9fea4ad7c289b122706640fd from imx_4.1.y:
    
    	MLK-11382 tty: serial: imx: disable irq after suspend
    
    	Disable rx irq after suspend to avoid interrupt coming in early
    	resume.
    
    	Signed-off-by: Fugang Duan <B38611@freescale.com>
    
    Signed-off-by: Leonard Crestez <leonard.crestez@nxp.com>

--- drivers/tty/serial/imx.c
+++ drivers/tty/serial/imx.c
@@ -2297,6 +2297,9 @@ static int imx_serial_port_suspend_noirq(struct device *dev)
 	if (ret)
 		return ret;
 
+	/* enable wakeup from i.MX UART */
+	serial_imx_enable_wakeup(sport, true);
+
 	serial_imx_save_context(sport);
 
 	clk_disable(sport->clk_ipg);
@@ -2320,6 +2323,9 @@ static int imx_serial_port_resume_noirq(struct device *dev)
 
 	serial_imx_restore_context(sport);
 
+	/* disable wakeup from i.MX UART */
+	serial_imx_enable_wakeup(sport, false);
+
 	clk_disable(sport->clk_ipg);
 
 	return 0;
@@ -2329,9 +2335,6 @@ static int imx_serial_port_suspend(struct device *dev)
 {
 	struct platform_device *pdev = to_platform_device(dev);
 	struct imx_port *sport = platform_get_drvdata(pdev);
-
-	/* enable wakeup from i.MX UART */
-	serial_imx_enable_wakeup(sport, true);
 
 	uart_suspend_port(&imx_reg, &sport->port);
 
@@ -2343,9 +2346,6 @@ static int imx_serial_port_resume(struct device *dev)
 {
 	struct platform_device *pdev = to_platform_device(dev);
 	struct imx_port *sport = platform_get_drvdata(pdev);
-
-	/* disable wakeup from i.MX UART */
-	serial_imx_enable_wakeup(sport, false);
 
 	uart_resume_port(&imx_reg, &sport->port);
 
