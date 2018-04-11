05d2076074509bc42a09700d06020211e8da26a3
Aymen Sghaier <aymen.sghaier@nxp.com>
2018-04-11 11:36:33 +0200

    MLK-17302: crypto: caam: Move RNG instantiation to SECO
     On i.MX8 platforms with SECO/SCU the RNG is not anymore instantiated
     by the Kernel driver but by SECO. This is true for B0 revision and
     later. A0 revision is not supported.
    
    Signed-off-by: Aymen Sghaier <aymen.sghaier@nxp.com>

--- drivers/crypto/caam/inst_rng.h
+++ drivers/crypto/caam/inst_rng.h
@@ -1,7 +1,7 @@
 /*
  * CAAM Private/internal definitions between modules
  *
- * Copyright 2017 NXP
+ * Copyright 2017-2018 NXP
  */
 
 #ifndef INST_RNG_H
@@ -10,9 +10,7 @@
 #include <linux/platform_device.h>
 
 #define ENT_DELAY_STEP	(400)
-int inst_rng_imx6(struct platform_device *pdev);
-
-int inst_rng_imx8(struct platform_device *pdev);
+int inst_rng_imx(struct platform_device *pdev);
 
 int deinst_rng(struct platform_device *pdev);
 
