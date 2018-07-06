caadb8c62d34c7fef5a61074f8b03b0048894bb7
Peng Fan <peng.fan@nxp.com>
2018-07-06 09:59:08 +0800

    MLK-18792-1 mmc: esdhc-sdhci-imx: no fail when no pinctrl available
    When using jailhouse to support two Linux on i.MX8MQ EVK,
    we use the 1st Linux to configure pinctrl for the 2nd Linux.
    Then the 2nd Linux could use the mmc without pinctrl driver.
    
    So give a warning message when no pinctrl available, but no fail probe.
    
    Signed-off-by: Peng Fan <peng.fan@nxp.com>

--- drivers/mmc/host/sdhci-esdhc-imx.c
+++ drivers/mmc/host/sdhci-esdhc-imx.c
@@ -1469,13 +1469,14 @@ static int sdhci_esdhc_imx_probe(struct platform_device *pdev)
 	imx_data->pinctrl = devm_pinctrl_get(&pdev->dev);
 	if (IS_ERR(imx_data->pinctrl)) {
 		err = PTR_ERR(imx_data->pinctrl);
-		goto disable_clk;
+		dev_warn(mmc_dev(host->mmc), "could not get pinctrl\n");
+		imx_data->pins_default = ERR_PTR(-EINVAL);
+	} else {
+		imx_data->pins_default = pinctrl_lookup_state(imx_data->pinctrl,
+							      PINCTRL_STATE_DEFAULT);
+		if (IS_ERR(imx_data->pins_default))
+			dev_warn(mmc_dev(host->mmc), "could not get default state\n");
 	}
-
-	imx_data->pins_default = pinctrl_lookup_state(imx_data->pinctrl,
-						PINCTRL_STATE_DEFAULT);
-	if (IS_ERR(imx_data->pins_default))
-		dev_warn(mmc_dev(host->mmc), "could not get default state\n");
 
 	if (imx_data->socdata->flags & ESDHC_FLAG_ENGCM07207)
 		/* Fix errata ENGcm07207 present on i.MX25 and i.MX35 */
