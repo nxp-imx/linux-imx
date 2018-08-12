895e0fac9129183b34acc6d7f93c681393a9896b
Fancy Fang <chen.fang@nxp.com>
2018-08-12 16:10:01 +0800

    MLK-19158-5 drm/imx: lcdif: add bus format check to CRTC's check
    Add the check to LCDIF CRTC atomic check for the requested bus
    format by encoder with the bus format which can be supported by
    LCDIF CRTC to refuse unsupported case.
    
    Signed-off-by: Fancy Fang <chen.fang@nxp.com>

--- drivers/gpu/drm/imx/lcdif/lcdif-crtc.c
+++ drivers/gpu/drm/imx/lcdif/lcdif-crtc.c
@@ -109,6 +109,24 @@ static const struct drm_crtc_funcs lcdif_crtc_funcs = {
 static int lcdif_crtc_atomic_check(struct drm_crtc *crtc,
 				   struct drm_crtc_state *state)
 {
+	struct lcdif_crtc *lcdif_crtc = to_lcdif_crtc(crtc);
+	struct imx_crtc_state *imx_crtc_state = to_imx_crtc_state(state);
+
+	/* check the requested bus format can be
+	 * supported by LCDIF CTRC or not
+	 */
+	switch (imx_crtc_state->bus_format) {
+	case MEDIA_BUS_FMT_RGB565_1X16:
+	case MEDIA_BUS_FMT_RGB666_1X18:
+	case MEDIA_BUS_FMT_RGB888_1X24:
+		break;
+	default:
+		dev_err(lcdif_crtc->dev,
+			"unsupported bus format: %#x\n",
+			imx_crtc_state->bus_format);
+		return -EINVAL;
+	}
+
 	return 0;
 }
 
