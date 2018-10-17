19e75645083219a1dad211790cfc33093064956e
Richard Liu <xuegang.liu@nxp.com>
2018-10-17 18:58:27 +0800

    MA-12873 Fix kernel panic due to NULL pointer access on imx6/7
    Meet kernel panic issue when run Surfaceview apk several hours
    on android. The panic is caused by NULL pointer access, root
    cause is still under checking, add the NULL pointer check can
    avoid the panic and don't see any side effect.
    
    Kernel panic log:
    Unable to handle kernel NULL pointer dereference at virtual address 000000a8
    pgd = d3938000
    [000000a8] *pgd=00000000
    Internal error: Oops: 5 [#1] PREEMPT SMP ARM
    Modules linked in: wlan
    CPU: 0 PID: 351 Comm: android.bg Not tainted 4.14.62 #1
    Hardware name: Freescale i.MX7ULP (Device Tree)
    task: d934ba00 task.stack: d939e000
    PC is at smaps_pte_range+0x380/0x3ec
    LR is at _raw_spin_unlock+0x24/0x50
    pc : [<c031d020>]    lr : [<c0c8d2f8>]    psr: 200f0013
    sp : d939fda8  ip : 00097719  fp : d939fe4c
    r10: f74616f0  r9 : b6480000  r8 : d8afed98
    r7 : b6480000  r6 : b6402000  r5 : ffefe008  r4 : d2978a00
    r3 : 00002000  r2 : 00000000  r1 : 000b6400  r0 : d8b49f60
    Flags: nzCv  IRQs on  FIQs on  Mode SVC_32  ISA ARM  Segment none
    Control: 10c53c7d  Table: 7393806a  DAC: 00000051
    
    Change-Id: Ib048c7973af09c0b2b03df2e4b664f5baea00902
    Signed-off-by: Richard Liu <xuegang.liu@nxp.com>

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -609,6 +609,9 @@ static void smaps_pte_entry(pte_t *pte, unsigned long addr,
 			page = device_private_entry_to_page(swpent);
 	} else if (unlikely(IS_ENABLED(CONFIG_SHMEM) && mss->check_shmem_swap
 							&& pte_none(*pte))) {
+		if (!vma->vm_file)
+			return;
+
 		page = find_get_entry(vma->vm_file->f_mapping,
 						linear_page_index(vma, addr));
 		if (!page)
