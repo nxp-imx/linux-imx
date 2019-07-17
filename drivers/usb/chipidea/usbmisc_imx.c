ffe9d9f338264ccd2ff92ef1037e62614cbc6d45
Li Jun <jun.li@nxp.com>
2019-07-17 16:51:20 +0800

    MLK-22280-2 usb: chipidea: usbmisc: remove mutex
    Now charger detection is only for iMX7 which has separated
    register area for each instance, register access is protected
    by spinlock among APIs, so we can remove the mutex.
    
    Acked-by: Peter Chen <peter.chen@nxp.com>
    Signed-off-by: Li Jun <jun.li@nxp.com>
    (cherry picked from commit 6f59f63011b5035c56bfb52180c4269b93b16fa1)

--- drivers/usb/chipidea/usbmisc_imx.c
+++ drivers/usb/chipidea/usbmisc_imx.c
@@ -186,7 +186,6 @@ struct imx_usbmisc {
 	void __iomem *base;
 	spinlock_t lock;
 	const struct usbmisc_ops *ops;
-	struct mutex mutex;
 };
 
 static struct regulator *vbus_wakeup_reg;
@@ -1093,7 +1092,6 @@ int imx_usbmisc_charger_detection(struct imx_usbmisc_data *data, bool connect)
 	if (!usbmisc->ops->charger_detection)
 		return -ENOTSUPP;
 
-	mutex_lock(&usbmisc->mutex);
 	if (connect) {
 		ret = usbmisc->ops->charger_detection(data);
 		if (ret) {
@@ -1108,7 +1106,6 @@ int imx_usbmisc_charger_detection(struct imx_usbmisc_data *data, bool connect)
 		usb_phy->chg_state = USB_CHARGER_ABSENT;
 		usb_phy->chg_type = UNKNOWN_TYPE;
 	}
-	mutex_unlock(&usbmisc->mutex);
 	return ret;
 }
 EXPORT_SYMBOL_GPL(imx_usbmisc_charger_detection);
@@ -1234,7 +1231,6 @@ static int usbmisc_imx_probe(struct platform_device *pdev)
 		return -ENOMEM;
 
 	spin_lock_init(&data->lock);
-	mutex_init(&data->mutex);
 
 	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
 	data->base = devm_ioremap_resource(&pdev->dev, res);
