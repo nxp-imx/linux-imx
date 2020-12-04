e7fc86e8b964ff54ec802f1afa73b126c92cc413
yang.tian <yang.tian@nxp.com>
2020-12-04 16:35:28 +0800

    MA-18246-3 Refine pm_qos ops in mxmdriver wifi driver
    Revert below two patches, since the pm_qos operations are
    refined in MXM5X16215 release to request pm_qos only in
    connected state.
    
    Revert "MA-18246-2 Refine pm_qos ops in mxmdriver wifi driver"
    This reverts commit d2bec32dee9148d8ce9c4fc062b9806adde7e89e.
    
    Revert "MA-18246 request/release pm_qos in mxmdriver wifi driver"
    This reverts commit c2f21cadd573473914f6caabd22c1a1f28a9648c.
    
    Change-Id: I4ea6bb6637ce810edea7846854158712189c082c
    Signed-off-by: yang.tian <yang.tian@nxp.com>

--- drivers/net/wireless/nxp/mxm_wifiex/wlan_src/mlinux/moal_main.c
+++ drivers/net/wireless/nxp/mxm_wifiex/wlan_src/mlinux/moal_main.c
@@ -65,11 +65,6 @@ Change log:
 #include <linux/of.h>
 #endif
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 35)
-#include <linux/pm_qos.h>
-#endif
-
-#include <linux/busfreq-imx.h>
 /********************************************************
 		 Global Variables
  ********************************************************/
@@ -90,13 +85,6 @@ extern int wifi_status;
 #if defined(STA_CFG80211) || defined(UAP_CFG80211)
 #if CFG80211_VERSION_CODE >= KERNEL_VERSION(3, 14, 0)
 extern int fw_region;
-#endif
-#endif
-
-#if LINUX_VERSION_CODE <= KERNEL_VERSION(5, 6, 0)
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 35)
-static struct pm_qos_request woal_pcie_pm_qos_req;
-int if_cnt = 0;
 #endif
 #endif
 /********************************************************
@@ -4369,27 +4357,9 @@ int woal_open(struct net_device *dev)
 
 	ENTER();
 
-	if (if_cnt == 0) {
-#if LINUX_VERSION_CODE <= KERNEL_VERSION(5, 6, 0)
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 35)
-		pm_qos_add_request(&woal_pcie_pm_qos_req, PM_QOS_CPU_DMA_LATENCY, 0);
-#endif
-#endif
-		request_bus_freq(BUS_FREQ_HIGH);
-	}
-	if_cnt++;
-
 	if (priv->phandle->surprise_removed == MTRUE) {
 		PRINTM(MERROR,
 		       "open is not allowed in surprise remove state.\n");
-		if (if_cnt > 0) {
-			release_bus_freq(BUS_FREQ_HIGH);
-#if LINUX_VERSION_CODE <= KERNEL_VERSION(5, 6, 0)
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 35)
-			pm_qos_remove_request(&woal_pcie_pm_qos_req);
-#endif
-#endif
-		}
 		LEAVE();
 		return -EFAULT;
 	}
@@ -4437,28 +4407,12 @@ int woal_open(struct net_device *dev)
 		if (i >= MAX_WAIT_DEVICE_READY_COUNT) {
 			PRINTM(MFATAL,
 			       "HW not ready, wlan_open() return failure\n");
-			if (if_cnt > 0) {
-				release_bus_freq(BUS_FREQ_HIGH);
-#if LINUX_VERSION_CODE <= KERNEL_VERSION(5, 6, 0)
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 35)
-				pm_qos_remove_request(&woal_pcie_pm_qos_req);
-#endif
-#endif
-			}
 			LEAVE();
 			return -EFAULT;
 		}
 	}
 #endif /* USB || SYSKT || SYSKT_MULTI */
 	if (!MODULE_GET) {
-		if (if_cnt > 0) {
-			release_bus_freq(BUS_FREQ_HIGH);
-#if LINUX_VERSION_CODE <= KERNEL_VERSION(5, 6, 0)
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 35)
-			pm_qos_remove_request(&woal_pcie_pm_qos_req);
-#endif
-#endif
-		}
 		LEAVE();
 		return -EFAULT;
 	}
@@ -4564,16 +4518,6 @@ int woal_close(struct net_device *dev)
 #endif /* < 2.6.34 */
 	}
 #endif /* USB_SUSPEND_RESUME */
-
-	if_cnt--;
-	if (if_cnt == 0) {
-		release_bus_freq(BUS_FREQ_HIGH);
-#if LINUX_VERSION_CODE <= KERNEL_VERSION(5, 6, 0)
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 35)
-		pm_qos_remove_request(&woal_pcie_pm_qos_req);
-#endif
-#endif
-	}
 
 	LEAVE();
 	return 0;
