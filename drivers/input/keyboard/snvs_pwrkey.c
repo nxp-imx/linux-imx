30c60e3b07cee5eede43e5af2e9a415a2b65eb47
Haoran.Wang <elven.wang@nxp.com>
2021-03-22 13:46:30 +0800

    Use Trusty OS handle SNVS RTC and pwrkey
    Use Trusty OS fast call handle SNVS RTC and pwrkey request.
    Prevent Linux access the registers directly.
    
    If Trusty OS ready for the SNVS RTC and pwrkey driver, then use
    smcall, or regmap access registers directly.
    
    Signed-off-by: Haoran.Wang <elven.wang@nxp.com>
    Change-Id: Id325a20f28a504f3f427a6ead69198529a321335

--- drivers/input/keyboard/snvs_pwrkey.c
+++ drivers/input/keyboard/snvs_pwrkey.c
@@ -14,11 +14,22 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/of.h>
+#include <linux/of_device.h>
 #include <linux/of_address.h>
 #include <linux/platform_device.h>
 #include <linux/pm_wakeirq.h>
 #include <linux/mfd/syscon.h>
 #include <linux/regmap.h>
+#include <linux/trusty/smcall.h>
+#include <linux/trusty/trusty.h>
+
+#define SMC_ENTITY_SNVS_RTC 53
+#define SMC_SNVS_PROBE SMC_FASTCALL_NR(SMC_ENTITY_SNVS_RTC, 0)
+#define SMC_SNVS_REGS_OP SMC_FASTCALL_NR(SMC_ENTITY_SNVS_RTC, 1)
+#define SMC_SNVS_LPCR_OP SMC_FASTCALL_NR(SMC_ENTITY_SNVS_RTC, 2)
+
+#define OPT_READ 0x1
+#define OPT_WRITE 0x2
 
 #define SNVS_LPSR_REG	0x4C	/* LP Status Register */
 #define SNVS_LPCR_REG	0x38	/* LP Control Register */
@@ -30,6 +41,19 @@
 #define DEBOUNCE_TIME 30
 #define REPEAT_INTERVAL 60
 
+
+static void trusty_snvs_update_lpcr(struct device *dev, u32 target, u32 enable) {
+		trusty_fast_call32(dev, SMC_SNVS_LPCR_OP, target, enable, 0);
+}
+
+static u32 trusty_snvs_read(struct device *dev, u32 target) {
+		return trusty_fast_call32(dev, SMC_SNVS_REGS_OP, target, OPT_READ, 0);
+}
+
+static void trusty_snvs_write(struct device *dev, u32 target, u32 value) {
+		trusty_fast_call32(dev, SMC_SNVS_REGS_OP, target, OPT_WRITE, value);
+}
+
 struct pwrkey_drv_data {
 	struct regmap *snvs;
 	int irq;
@@ -40,6 +64,7 @@ struct pwrkey_drv_data {
 	struct clk *clk;
 	struct timer_list check_timer;
 	struct input_dev *input;
+	struct device *trusty_dev;
 };
 
 static void imx_imx_snvs_check_for_events(struct timer_list *t)
@@ -55,7 +80,10 @@ static void imx_imx_snvs_check_for_events(struct timer_list *t)
 			clk_enable(pdata->clk);
 	}
 
-	regmap_read(pdata->snvs, SNVS_HPSR_REG, &state);
+	if (pdata->trusty_dev)
+		state = trusty_snvs_read(pdata->trusty_dev, SNVS_HPSR_REG);
+	else
+		regmap_read(pdata->snvs, SNVS_HPSR_REG, &state);
 
 	if (pdata->clk) {
 		if (pdata->suspended)
@@ -99,12 +127,19 @@ static irqreturn_t imx_snvs_pwrkey_interrupt(int irq, void *dev_id)
 		input_sync(input);
 	}
 
-	regmap_read(pdata->snvs, SNVS_LPSR_REG, &lp_status);
+	if (pdata->trusty_dev != NULL)
+		lp_status = trusty_snvs_read(pdata->trusty_dev, SNVS_LPSR_REG);
+	else
+		regmap_read(pdata->snvs, SNVS_LPSR_REG, &lp_status);
+
 	if (lp_status & SNVS_LPSR_SPO)
 		mod_timer(&pdata->check_timer, jiffies + msecs_to_jiffies(DEBOUNCE_TIME));
 
 	/* clear SPO status */
-	regmap_write(pdata->snvs, SNVS_LPSR_REG, SNVS_LPSR_SPO);
+	if (pdata->trusty_dev != NULL)
+		trusty_snvs_write(pdata->trusty_dev, SNVS_LPSR_REG, SNVS_LPSR_SPO);
+	else
+		regmap_write(pdata->snvs, SNVS_LPSR_REG, SNVS_LPSR_SPO);
 
 	if (pdata->clk)
 		clk_disable(pdata->clk);
@@ -125,6 +160,8 @@ static int imx_snvs_pwrkey_probe(struct platform_device *pdev)
 	struct input_dev *input = NULL;
 	struct device_node *np;
 	int error;
+	struct device_node *sp;
+	struct platform_device * pd;
 
 	/* Get SNVS register Page */
 	np = pdev->dev.of_node;
@@ -135,10 +172,32 @@ static int imx_snvs_pwrkey_probe(struct platform_device *pdev)
 	if (!pdata)
 		return -ENOMEM;
 
+	sp = of_find_node_by_name(NULL, "trusty");
+
+	if (sp != NULL) {
+		pd = of_find_device_by_node(sp);
+		if (pd != NULL) {
+			pdata->trusty_dev = &(pd->dev);
+			dev_err(&pdev->dev, "snvs pwkey: get trusty_dev node, use Trusty mode.\n");
+		} else
+			dev_err(&pdev->dev, "snvs pwkey: failed to get trusty_dev node\n");
+	} else {
+		dev_err(&pdev->dev, "snvs pwkey: failed to find trusty node. Use normal mode.\n");
+		pdata->trusty_dev = NULL;
+	}
+
 	pdata->snvs = syscon_regmap_lookup_by_phandle(np, "regmap");
 	if (IS_ERR(pdata->snvs)) {
 		dev_err(&pdev->dev, "Can't get snvs syscon\n");
 		return PTR_ERR(pdata->snvs);
+	}
+
+	if (pdata->trusty_dev) {
+		error = trusty_fast_call32(pdata->trusty_dev, SMC_SNVS_PROBE, 0, 0, 0);
+		if (error < 0) {
+			dev_err(&pdev->dev, "snvs pwkey trusty dev failed to probe!nr=0x%x error=%d\n", SMC_SNVS_PROBE, error);
+			pdata->trusty_dev = NULL;
+		}
 	}
 
 	if (of_property_read_u32(np, "linux,keycode", &pdata->keycode)) {
@@ -164,10 +223,16 @@ static int imx_snvs_pwrkey_probe(struct platform_device *pdev)
 		}
 	}
 
-	regmap_update_bits(pdata->snvs, SNVS_LPCR_REG, SNVS_LPCR_DEP_EN, SNVS_LPCR_DEP_EN);
+	if (pdata->trusty_dev != NULL)
+		trusty_snvs_update_lpcr(pdata->trusty_dev, SNVS_LPCR_DEP_EN, 1);
+	else
+		regmap_update_bits(pdata->snvs, SNVS_LPCR_REG, SNVS_LPCR_DEP_EN, SNVS_LPCR_DEP_EN);
 
 	/* clear the unexpected interrupt before driver ready */
-	regmap_write(pdata->snvs, SNVS_LPSR_REG, SNVS_LPSR_SPO);
+	if (pdata->trusty_dev != NULL)
+		trusty_snvs_write(pdata->trusty_dev, SNVS_LPSR_REG, SNVS_LPSR_SPO);
+	else
+		regmap_write(pdata->snvs, SNVS_LPSR_REG, SNVS_LPSR_SPO);
 
 	timer_setup(&pdata->check_timer, imx_imx_snvs_check_for_events, 0);
 
