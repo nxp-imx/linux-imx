e0bc859c3672f81abe7a80b851e2ea30771b67d0
zhang sanshan <pete.zhang@nxp.com>
2021-07-20 14:41:43 +0800

    MA-19289-1 GKI: v4l2-subdev: Remove obsolete g/s_parm
    Revert "Revert "media: v4l2-subdev.h: remove obsolete g/s_parm""
    
    This reverts commit f277267df0943b707617e309beffbbf2017d9cf8.
    
    Change-Id: Id749c18451705081f8215d938c8173b253465b8f

--- include/media/v4l2-subdev.h
+++ include/media/v4l2-subdev.h
@@ -461,9 +461,6 @@ enum v4l2_subdev_pre_streamon_flags {
  *
  * @g_pixelaspect: callback to return the pixelaspect ratio.
  *
- * @g_parm: callback for VIDIOC_G_PARM() ioctl handler code.
- *
- * @s_parm: callback for VIDIOC_S_PARM() ioctl handler code.
  *
  * @s_rx_buffer: set a host allocated memory buffer for the subdev. The subdev
  *	can adjust @size to a lower value and must not write more data to the
@@ -495,8 +492,6 @@ struct v4l2_subdev_video_ops {
 	int (*g_input_status)(struct v4l2_subdev *sd, u32 *status);
 	int (*s_stream)(struct v4l2_subdev *sd, int enable);
 	int (*g_pixelaspect)(struct v4l2_subdev *sd, struct v4l2_fract *aspect);
-	int (*g_parm)(struct v4l2_subdev *sd, struct v4l2_streamparm *param);
-	int (*s_parm)(struct v4l2_subdev *sd, struct v4l2_streamparm *param);
 	int (*s_rx_buffer)(struct v4l2_subdev *sd, void *buf,
 			   unsigned int *size);
 	int (*pre_streamon)(struct v4l2_subdev *sd, u32 flags);
