413e652f63e9dae09fd32e8ab580f0870e17a3fc
Guoniu.zhou <guoniu.zhou@nxp.com>
2022-10-12 18:24:25 +0800

    iLF-7244-05: media: i2c: ov5640: fix the dark image issue
    The first frame will have dark issue since auto exposure is not
    stable in the previous frames, so add 100ms delay to skip the
    first three frames.
    
    Change-Id: I2d2a2f0b6b407f751a6cb0a3d84ba1e3e8bbdc93
    Signed-off-by: Guoniu.zhou <guoniu.zhou@nxp.com>
    Reviewed-by: Robby Cai <robby.cai@nxp.com>

--- drivers/media/i2c/ov5640.c
+++ drivers/media/i2c/ov5640.c
@@ -3760,6 +3760,7 @@ static int ov5640_enum_mbus_code(struct v4l2_subdev *sd,
 static int ov5640_s_stream(struct v4l2_subdev *sd, int enable)
 {
 	struct ov5640_dev *sensor = to_ov5640_dev(sd);
+	int delay;
 	int ret = 0;
 
 	if (enable) {
@@ -3797,6 +3798,10 @@ static int ov5640_s_stream(struct v4l2_subdev *sd, int enable)
 
 		if (!ret)
 			sensor->streaming = enable;
+
+		/* wait camera stable */
+		delay = (enable) ? 100 : 0;
+		msleep(delay);
 	}
 
 out:
