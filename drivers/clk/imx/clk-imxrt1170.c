4f0735b60233ec98ed75680b1100ea44fd1141b1
Vladimir Skvortsov <vskvortsov@emcraft.com>
2023-08-09 14:42:19 +0300

    RM 6733. clk:imxrt1170: Add gate for the USB clocks.
    (cherry picked from commit 3291b1ea2d0812b16e2c1c032f2b70e1866d82d8)

--- drivers/clk/imx/clk-imxrt1170.c
+++ drivers/clk/imx/clk-imxrt1170.c
@@ -101,6 +101,7 @@ static struct imxrt1170_clk_ccgr clk_ccgrs[] = {
         { IMXRT1170_CLK_GPT1, "gpt1", "gpt1_root", (0x6000 + (64 * 0x20)), },
 	{ IMXRT1170_CLK_LPUART1, "lpuart1", "lpuart1_root", (0x6000 + (86 * 0x20)), },
 	{ IMXRT1170_CLK_ENET1, "enet1", "enet1_root", (0x6000 + (112 * 0x20)), },
+	{ IMXRT1170_CLK_USB, "usb", "bus_root", (0x6000 + (115 * 0x20)), },
 	{ IMXRT1170_CLK_USDHC1, "usdhc1", "usdhc1_root", (0x6000 + (117 * 0x20)), },
 };
 
