66d76bb02dbd63145323afc46d767c3f96b93f64
Vladimir Skvortsov <vskvortsov@emcraft.com>
2023-11-21 12:31:18 +0300

    RM-6916: clk: imxrt1050: Make AHB clock critical for the IMXRT1050 target to prevent boot issues if the USDHC controller is disabled in DTS.
    (cherry picked from commit 6a2159f42c5b3f5ffa7402d4a7a28fbd983c0488)
    (cherry picked from commit 65e51bb7c365c57a0f76d16b63dfc2692264e08c)
    (cherry picked from commit f6f23ee861ce310c3b900e17c4bc6183b773a3b6)

--- drivers/clk/imx/clk-imxrt1050.c
+++ drivers/clk/imx/clk-imxrt1050.c
@@ -167,7 +167,7 @@ static int imxrt1050_clocks_probe(struct platform_device *pdev)
 	hws[IMXRT1050_CLK_SEMC_SEL] = imx_clk_hw_mux_flags("semc_sel", ccm_base + 0x14, 6, 1,
 		semc_sels, ARRAY_SIZE(semc_sels), CLK_IS_CRITICAL);
 
-	hws[IMXRT1050_CLK_AHB_PODF] = imx_clk_hw_divider("ahb", "periph_sel", ccm_base + 0x14, 10, 3);
+	hws[IMXRT1050_CLK_AHB_PODF] = imx_clk_hw_divider_flags("ahb", "periph_sel", ccm_base + 0x14, 10, 3, CLK_IS_CRITICAL);
 	hws[IMXRT1050_CLK_IPG_PODF] = imx_clk_hw_divider("ipg", "ahb", ccm_base + 0x14, 8, 2);
 	hws[IMXRT1050_CLK_PER_PODF] = imx_clk_hw_divider("per", "per_sel", ccm_base + 0x1C, 0, 5);
 
