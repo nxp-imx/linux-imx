4bc5e18959556f9556943e02e7490d9966c651cd
Zhipeng Wang <zhipeng.wang_1@nxp.com>
2023-11-22 15:32:00 +0900

    MA-21882-3 clk: imx: move imx_src_is_m4_enabled to clk
    `imx_src_is_m4_enabled` is used by `clk-provider` and `soc-imx8m`
    enables clk during initialization, which creates a mutual dependency
    between the `soc-imx8m` module and the `clk-provider` module. If both
    are converted into modules, loading either one first will cause issues.
    
    Change-Id: I728eed92b34eaf97f75a68270ca043947a1c0eab
    Signed-off-by: Zhipeng Wang <zhipeng.wang_1@nxp.com>

--- drivers/soc/imx/soc-imx8m.c
+++ drivers/soc/imx/soc-imx8m.c
@@ -3,7 +3,6 @@
  * Copyright 2019 NXP.
  */
 
-#include <linux/arm-smccc.h>
 #include <linux/init.h>
 #include <linux/io.h>
 #include <linux/module.h>
@@ -16,7 +15,6 @@
 #include <linux/clk.h>
 
 #include <soc/imx/soc.h>
-#include <soc/imx/src.h>
 
 #define REV_B1				0x21
 
@@ -378,33 +376,3 @@ static int __init imx8_soc_init(void)
 device_initcall(imx8_soc_init);
 MODULE_DESCRIPTION("NXP i.MX8M SoC driver");
 MODULE_LICENSE("GPL");
-
-#define FSL_SIP_SRC                    0xc2000005
-#define FSL_SIP_SRC_M4_START           0x00
-#define FSL_SIP_SRC_M4_STARTED         0x01
-
-/* To indicate M4 enabled or not on i.MX8MQ */
-static bool m4_is_enabled;
-bool imx_src_is_m4_enabled(void)
-{
-	return m4_is_enabled;
-}
-EXPORT_SYMBOL_GPL(imx_src_is_m4_enabled);
-
-int check_m4_enabled(void)
-{
-	struct arm_smccc_res res;
-
-	arm_smccc_smc(FSL_SIP_SRC, FSL_SIP_SRC_M4_STARTED, 0,
-		      0, 0, 0, 0, 0, &res);
-		      m4_is_enabled = !!res.a0;
-
-	if (m4_is_enabled)
-		printk("M4 is started\n");
-
-	return 0;
-}
-EXPORT_SYMBOL_GPL(check_m4_enabled);
-
-MODULE_DESCRIPTION("i.MX8M SoC driver");
-MODULE_LICENSE("GPL v2");
