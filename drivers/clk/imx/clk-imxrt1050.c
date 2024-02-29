5f2ba820a7b583e10b321497945f09c760541b01
Vladimir Skvortsov <vskvortsov@emcraft.com>
2024-02-29 17:11:54 +0300

    RM-6971: ARM: DTS: IMXRT1050: Define clocks and DTS nodes for the instances #2 and #3 of the i.MXRT1050 LPUART controllers.
    (cherry picked from commit 709643cca1703978d150b67d18be98cf556ae571)
    (cherry picked from commit 9726afcdde34a0a0a706878c6ee8735c585602ec)
    (cherry picked from commit c356c5f0def137869a6881cfed345e1cbc2b8877)

--- drivers/clk/imx/clk-imxrt1050.c
+++ drivers/clk/imx/clk-imxrt1050.c
@@ -182,6 +182,8 @@ static int imxrt1050_clocks_probe(struct platform_device *pdev)
 	hws[IMXRT1050_CLK_USDHC1] = imx_clk_hw_gate2("usdhc1", "usdhc1_podf", ccm_base + 0x80, 2);
 	hws[IMXRT1050_CLK_USDHC2] = imx_clk_hw_gate2("usdhc2", "usdhc2_podf", ccm_base + 0x80, 4);
 	hws[IMXRT1050_CLK_LPUART1] = imx_clk_hw_gate2("lpuart1", "lpuart_podf", ccm_base + 0x7c, 24);
+	hws[IMXRT1050_CLK_LPUART2] = imx_clk_hw_gate2("lpuart2", "lpuart_podf", ccm_base + 0x68, 28);
+	hws[IMXRT1050_CLK_LPUART3] = imx_clk_hw_gate2("lpuart3", "lpuart_podf", ccm_base + 0x68, 12);
 	hws[IMXRT1050_CLK_LCDIF_APB] = imx_clk_hw_gate2("lcdif", "lcdif_podf", ccm_base + 0x70, 28);
 	hws[IMXRT1050_CLK_LCDIF_PIX] = imx_clk_hw_gate2("lcdif_pix", "lcdif", ccm_base + 0x74, 10);
 	hws[IMXRT1050_CLK_DMA] = imx_clk_hw_gate("dma", "ipg", ccm_base + 0x7C, 6);
