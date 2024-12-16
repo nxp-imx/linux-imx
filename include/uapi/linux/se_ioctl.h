b34e288b1e634855b0b0c48f6d9a12a78e0f2144
Zhipeng Wang <zhipeng.wang_1@nxp.com>
2024-12-16 18:47:48 +0900

    MA-23218 drivers: firmware: imx: Add __kernel_timespec for user space
    Add __kernel_timespec in relevant structures and IOCTL handling
    to make it compatible with user space.
    
    Change-Id: I7a49f7b61490a176d92cc66dad160e1a71d7454a
    Signed-off-by: Zhipeng Wang <zhipeng.wang_1@nxp.com>

--- include/uapi/linux/se_ioctl.h
+++ include/uapi/linux/se_ioctl.h
@@ -7,6 +7,7 @@
 #define SE_IOCTL_H
 
 #include <linux/types.h>
+#include <linux/time_types.h>
 
 #define SE_TYPE_STR_DBG			"dbg"
 #define SE_TYPE_STR_HSM			"hsm"
@@ -71,8 +72,13 @@ struct se_ioctl_signed_message {
 };
 
 struct se_time_frame {
+#ifdef __KERNEL__
 	struct timespec64 t_start;
 	struct timespec64 t_end;
+#else
+	struct __kernel_timespec t_start;
+	struct __kernel_timespec t_end;
+#endif
 };
 
 /* IO Buffer Flags */
