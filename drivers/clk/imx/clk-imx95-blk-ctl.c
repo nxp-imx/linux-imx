f5b1c6c07e04681483978d66aa92b33ff2af4124
Liu Ying <victor.liu@nxp.com>
2025-01-03 12:46:25 +0800

    LF-17068-2 clk: imx95-blk-ctl: Add i.MX952 display CSR
    Add i.MX952 display CSR as a syscon and also as a parent device to
    populate pixel link child device.
    
    Reviewed-by: Laurentiu Palcu <laurentiu.palcu@oss.nxp.com>
    Signed-off-by: Liu Ying <victor.liu@nxp.com>

--- drivers/clk/imx/clk-imx95-blk-ctl.c
+++ drivers/clk/imx/clk-imx95-blk-ctl.c
@@ -547,6 +547,7 @@ static const struct of_device_id imx95_bc_of_match[] = {
 	{ .compatible = "nxp,imx95-hsio-blk-ctl", .data = &hsio_blk_ctl_dev_data },
 	{ .compatible = "nxp,imx95-vpu-csr", .data = &vpublk_dev_data },
 	{ .compatible = "nxp,imx95-netcmix-blk-ctrl", .data = &netcmix_dev_data},
+	{ .compatible = "nxp,imx952-display-csr", },
 	{ /* Sentinel */ },
 };
 MODULE_DEVICE_TABLE(of, imx95_bc_of_match);
