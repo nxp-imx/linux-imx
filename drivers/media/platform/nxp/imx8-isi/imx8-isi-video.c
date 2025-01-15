a0c08e9b935a3af64bc3a0106ff2c5208fe43f6d
Fang Hui <hui.fang@nxp.com>
2025-01-15 22:02:11 +0800

    MA-17941 Workaround ISI overwrite issue on 6.12 kernel
    8mn/8qxp/8qm/8mp may has the issue, double the discard buffer size.
    Currently, ov5640 on 8mp use drivers in "drivers/staging/media/imx",
    already have related patch. In case in future move to this driver, also
    apply the tactic on 8mp.
    
    Change-Id: I1245fa00d87d55eb057f67cdf57cea1efcd1f140
    Signed-off-by: Fang Hui <hui.fang@nxp.com>

--- drivers/media/platform/nxp/imx8-isi/imx8-isi-video.c
+++ drivers/media/platform/nxp/imx8-isi/imx8-isi-video.c
@@ -759,12 +759,21 @@ static void mxc_isi_video_free_discard_buffers(struct mxc_isi_video *video)
 static int mxc_isi_video_alloc_discard_buffers(struct mxc_isi_video *video)
 {
 	unsigned int i, j;
+	const struct mxc_isi_plat_data *pdata = video->pipe->isi->pdata;
 
 	/* Allocate memory for each plane. */
 	for (i = 0; i < video->pix.num_planes; i++) {
 		struct mxc_isi_dma_buffer *buf = &video->discard_buffer[i];
-
 		buf->size = PAGE_ALIGN(video->pix.plane_fmt[i].sizeimage);
+
+		if ((pdata->model == MXC_ISI_IMX8MN) || (pdata->model == MXC_ISI_IMX8QM) \
+			|| (pdata->model == MXC_ISI_IMX8QXP) || (pdata->model == MXC_ISI_IMX8MP)) {
+			buf->size *= 2;
+			dev_info(video->pipe->isi->dev,
+				"double the size from %zu to %zu\n",
+				buf->size/2, buf->size);
+		}
+
 		buf->addr = dma_alloc_coherent(video->pipe->isi->dev, buf->size,
 					       &buf->dma, GFP_DMA | GFP_KERNEL);
 		if (!buf->addr) {
