81de4fc3aac8b5f1bc4708c1876a1a34045e27c4
Alice Guo <alice.guo@nxp.com>
2025-05-22 10:31:06 +0800

    LF-17215-1 nvmem: imx952: Add ocotp fuse box driver for i.MX952
    Ocotp map entry is according to
    i.MX952_Fusemap_v1.02_Documentation_burn_plan.
    
    Signed-off-by: Alice Guo <alice.guo@nxp.com>
    Reviewed-by: Peng Fan <peng.fan@nxp.com>

--- drivers/nvmem/imx-ocotp-ele.c
+++ drivers/nvmem/imx-ocotp-ele.c
@@ -435,11 +435,35 @@ static const struct ocotp_devtype_data imx95_ocotp_data = {
 	},
 };
 
+static const struct ocotp_devtype_data imx952_ocotp_data = {
+	.reg_off = 0x8000,
+	.reg_read = imx_ocotp_reg_read,
+	.reg_write = imx_ocotp_reg_write,
+	.size = 2440, /* 610 words */
+	.num_entry = 12,
+	.se_soc_id = SOC_ID_OF_IMX952,
+	.entry = {
+		{ 0, 1, FUSE_FSB | FUSE_ECC },
+		{ 7, 1, FUSE_FSB | FUSE_ECC },
+		{ 9, 3, FUSE_FSB | FUSE_ECC },
+		{ 12, 24, FUSE_FSB },
+		{ 36, 2, FUSE_FSB  | FUSE_ECC },
+		{ 38, 14, FUSE_FSB },
+		{ 317, 2, FUSE_FSB | FUSE_ECC },
+		{ 320, 7, FUSE_FSB },
+		{ 328, 64, FUSE_FSB },
+		{ 448, 143, FUSE_FSB },
+		{ 591, 1, FUSE_FSB | FUSE_ECC },
+		{ 592, 16, FUSE_FSB }
+	},
+};
+
 static const struct of_device_id imx_ele_ocotp_dt_ids[] = {
 	{ .compatible = "fsl,imx8ulp-ocotp", .data = &imx8ulp_ocotp_data, },
 	{ .compatible = "fsl,imx93-ocotp", .data = &imx93_ocotp_data, },
 	{ .compatible = "fsl,imx94-ocotp", .data = &imx94_ocotp_data, },
 	{ .compatible = "fsl,imx95-ocotp", .data = &imx95_ocotp_data, },
+	{ .compatible = "fsl,imx952-ocotp", .data = &imx952_ocotp_data, },
 	{},
 };
 MODULE_DEVICE_TABLE(of, imx_ele_ocotp_dt_ids);
