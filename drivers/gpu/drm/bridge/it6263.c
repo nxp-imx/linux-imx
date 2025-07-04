5de3633f8588f2c38a20807c5388c1ac29271777
Bo Zhang <bo.zhang@nxp.com>
2025-07-04 12:14:08 +0900

    MA-23890-1 Wait some time before get connection status
    The fbdev emulation will get the connector connection status when init and
    register. While the connection status of it6263 isn't updated in time after
    driver probed. Need to wait some time after initialization before get connection
    status.
    
    Change-Id: I22ff13113db3933dccdac3ea13839ed9ac18d898
    Signed-off-by: Bo Zhang <bo.zhang@nxp.com>

--- drivers/gpu/drm/bridge/it6263.c
+++ drivers/gpu/drm/bridge/it6263.c
@@ -1094,6 +1094,12 @@ static int it6263_probe(struct i2c_client *client)
 
 	i2c_set_clientdata(client, it6263);
 
+#if defined(CONFIG_DRM_FBDEV_EMULATION) && defined(CONFIG_LOGO)
+	/* Waiting some time after init, so it6263_bridge_detect() can get correct
+	 * status. It will help to show Linux logo(penguins) normally.
+	 */
+	usleep_range(55000, 56000);
+#endif
 	return ret;
 
 unregister_lvds_i2c:
