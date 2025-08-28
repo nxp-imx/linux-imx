e6f466a25b7c9705d345adc9552d02696ac6c48a
Jiyu Yang <jiyu.yang@nxp.com>
2025-08-28 14:05:49 +0800

    MGS-8396 gpu: avoid the race when suspend/resume
    This also reverts commit 9c64435b703b4952d836fd7349ab73cd6d1dbd3b.
    
    Change-Id: Ic15fe7954eebff386e044ae62f101ed52c949fc5
    Signed-off-by: Jiyu Yang <jiyu.yang@nxp.com>

--- drivers/gpu/arm/midgard/csf/mali_kbase_csf_defs.h
+++ drivers/gpu/arm/midgard/csf/mali_kbase_csf_defs.h
@@ -1256,9 +1256,6 @@ struct kbase_csf_protm_mem_pages_defer_ctrl {
  *                          perform a scheduling tock.
  * @pending_gpu_idle_work:  Indicates that kbase_csf_scheduler_kthread() should
  *                          handle the GPU IDLE event.
- * @pending_runtime_suspend_work: Indicates that kbase_csf_scheduler_kthread()
- *                                should proceed to suspend the GPU as part of
- *                                handling the runtime suspend event.
  * @pending_power_off_work: Indicates that kbase_csf_scheduler_kthread() should
  *                          proceed to power off the GPU.
  * @ping_work:              Work item that would ping the firmware at regular
@@ -1375,7 +1372,6 @@ struct kbase_csf_scheduler {
 	atomic_t pending_tick_work;
 	atomic_t pending_tock_work;
 	atomic_t pending_gpu_idle_work;
-	atomic_t pending_runtime_suspend_work;
 	atomic_t pending_power_off_work;
 	struct delayed_work ping_work;
 	struct kbase_context *top_kctx;
