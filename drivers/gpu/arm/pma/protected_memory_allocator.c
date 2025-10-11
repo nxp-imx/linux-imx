03ef7e2c7b698b815f9ef99e59f5f22edbbe869d
Zhipeng Wang <zhipeng.wang_1@nxp.com>
2025-10-11 15:20:12 +0900

    MA-24131 gpu: revert gpu pma related patches
    gpu pma is not yet ready on Android, so temporarily revert the related
    patches until proper support is available:
    
    3cad4802586c Revert "MGS-8374-4: gpu: add pma dts"
    afc9f9704007 Revert "MGS-8374-3: gpu: add pma in imx_v8_defconfig"
    f3e811ce5041 Revert "MGS-8374-2: gpu: add pma/mgm Kbuild"
    735b6dfd28d0 Revert "MGS-8374-1: gpu: update MAINTAIN directory"
    
    Change-Id: I19d178441fad963abdb0aea69d71bdba0d1dbfcf

--- drivers/gpu/arm/pma/protected_memory_allocator.c
+++ drivers/gpu/arm/pma/protected_memory_allocator.c
@@ -2,7 +2,6 @@
 /*
  *
  * (C) COPYRIGHT 2019-2024 ARM Limited. All rights reserved.
- * COPYRIGHT 2024 - 2025 NXP
  *
  * This program is free software and is provided to you under the terms of the
  * GNU General Public License version 2 as published by the Free Software
@@ -305,7 +304,7 @@ simple_pma_alloc_page(struct protected_memory_allocator_device *pma_dev, unsigne
 
 					large_granularity_alloc(epma_dev, start_idx, order, pma);
 
-					epma_dev->num_free_pages -= 1ULL << order;
+					epma_dev->num_free_pages -= 1 << order;
 					spin_unlock(&epma_dev->rmem_lock);
 					return pma;
 				}
@@ -542,6 +541,7 @@ out_err:
 	return;
 #endif
 }
+#endif
 
 static const struct of_device_id protected_memory_allocator_dt_ids[] = {
 	{ .compatible = "arm,protected-memory-allocator" },
