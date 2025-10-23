36a28b1a1bc1ac168ce0682d2d5e35b92d8bf949
Zhipeng Wang <zhipeng.wang_1@nxp.com>
2025-10-23 16:49:23 +0900

    MA-24156 Revert "LF-15984: drm: bridge: dw_hdmi: Add a sysfs attribute file to control delay"
    This patch adds a sysfs attribute file in the Synopsys DesignWare HDMI
    CEC driver to control the retransmission delay of CEC messages. However,
    it modifies the common structure cec_adapter and breaks the KMI.
    Therefore, it should be reverted.
    
    This reverts commit 0a449a2a2ec406fdfff431405a008864f312b7ea.
    
    Change-Id: I4fcc39611241d8398e72daf6ab40eba68592e69b

--- include/media/cec.h
+++ include/media/cec.h
@@ -229,8 +229,6 @@ struct cec_adap_ops {
  * @cec_dir:		debugfs cec directory
  * @sequence:		transmit sequence counter
  * @input_phys:		remote control input_phys name
- * @eom_delay:		custom delay (us) before sending a reply
- *                      to CEC_MSG_FEATURE_ABORT
  *
  * This structure represents a cec adapter.
  */
@@ -298,7 +296,6 @@ struct cec_adapter {
 	u32 sequence;
 
 	char input_phys[40];
-	u32 eom_delay;
 };
 
 static inline int cec_get_device(struct cec_adapter *adap)
