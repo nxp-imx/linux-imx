c64196b9997ff5900541027663ff4b01eaa7b9db
Zhipeng Wang <zhipeng.wang_1@nxp.com>
2025-12-17 18:56:11 +0900

    MA-24293 phy: freescale: imx8m-pcie: Convert to module_param for modular build
    Replace __setup() with module_param() to fix unused function warning
    when the driver is built as a module.
    
    The __setup() mechanism only works when the driver is built into the
    kernel, causing imx8_pcie_phy_fine_tune() to become an unused function
    when compiled as a module, which triggers a compilation error with
    -Werror,-Wunused-function.
    
    Using module_param() allows the parameter to work in both built-in and
    modular configurations. The parameter can now be set via:
    - Module load: modprobe phy-fsl-imx8m-pcie imx8_pcie_phy_tuned=1
    - Kernel cmdline: phy-fsl-imx8m-pcie.imx8_pcie_phy_tuned=1
    
    This maintains the same functionality for enabling PCIe PHY fine tuning
    in i.MX8MP EP/RC validation systems to achieve Gen3 link speeds.
    
    Change-Id: I3ff490784ba82c9f1c44b12abdd95ec92046f996
    Signed-off-by: Zhipeng Wang <zhipeng.wang_1@nxp.com>

--- drivers/phy/freescale/phy-fsl-imx8m-pcie.c
+++ drivers/phy/freescale/phy-fsl-imx8m-pcie.c
@@ -284,16 +284,8 @@ static const struct of_device_id imx8_pcie_phy_of_match[] = {
 };
 MODULE_DEVICE_TABLE(of, imx8_pcie_phy_of_match);
 
-static int __init imx8_pcie_phy_fine_tune(char *str)
-{
-	if (!strcmp(str, "yes")) {
-		pr_info("i.MX PCIe PHY is fine tuned in EP/RC SYS.\n");
-		imx8_pcie_phy_tuned = 1;
-	}
-	return 1;
-}
-
-__setup("pcie_phy_tuned=", imx8_pcie_phy_fine_tune);
+module_param(imx8_pcie_phy_tuned, int, 0644);
+MODULE_PARM_DESC(imx8_pcie_phy_tuned, "Enable i.MX PCIe PHY fine tuning for EP/RC system (0=disabled, 1=enabled)");
 
 static int imx8_pcie_phy_probe(struct platform_device *pdev)
 {
