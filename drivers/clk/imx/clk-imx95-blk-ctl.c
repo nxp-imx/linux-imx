7f9525b3d128ebf41f66defa5796e21c3c71eda4
Richard Zhu <hongxing.zhu@nxp.com>
2025-12-26 09:53:44 +0800

    LF-17053-5 clk: imx95-blk-ctl: Add one more refout_en clock for i.MX952 PCIe
    Add one more refout_en clock for i.MX952 PCIe, to enable output of
    internal 100M differential reference clock.
    
    A new change in MX952 as compared to MX95 related to RECLK_OUT:
    HSIO_BLK_CTRL_LFAST_IO_REG (addr=0x4C0100C0)  :: Set Bit2 (FUNC_OUTPUT_EN) & Bit6 (CREF_EN) to 1.
    
    Signed-off-by: Richard Zhu <hongxing.zhu@nxp.com>
    Reviewed-by: Frank Li <Frank.Li@nxp.com>

--- drivers/clk/imx/clk-imx95-blk-ctl.c
+++ drivers/clk/imx/clk-imx95-blk-ctl.c
@@ -287,11 +287,21 @@ static const struct imx95_blk_ctl_clk_dev_data hsio_blk_ctl_clk_dev_data[] = {
 		.bit_width = 1,
 		.type = CLK_GATE,
 		.flags = CLK_SET_RATE_PARENT,
+	},
+	[1] = {
+		.name = "hsio_blk_ctl_clk1",
+		.parent_names = (const char *[]){ "hsio_pll", },
+		.num_parents = 1,
+		.reg = 0,
+		.bit_idx = 2,
+		.bit_width = 1,
+		.type = CLK_GATE,
+		.flags = CLK_SET_RATE_PARENT,
 	}
 };
 
 static const struct imx95_blk_ctl_dev_data hsio_blk_ctl_dev_data = {
-	.num_clks = 1,
+	.num_clks = ARRAY_SIZE(hsio_blk_ctl_clk_dev_data),
 	.clk_dev_data = hsio_blk_ctl_clk_dev_data,
 };
 
