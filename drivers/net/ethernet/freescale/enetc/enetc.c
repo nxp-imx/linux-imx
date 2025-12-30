e083ce2ed34182200b70a83173a5bdde8cf0f3b4
Clark Wang <xiaoning.wang@nxp.com>
2025-12-30 17:38:12 +0800

    LF-17107-4 net: enetc: add 400MHz sysclk support for imx952
    To enable SGMII, currently, 400MHz is required as the sysclk of enetc.
    Depending on different IP revisions, change the sysclk_freq in the
    enetc_drvdata used by imx952 to 400MHz.
    
    Signed-off-by: Clark Wang <xiaoning.wang@nxp.com>
    Reviewed-by: Wei Fang <wei.fang@nxp.com>

--- drivers/net/ethernet/freescale/enetc/enetc.c
+++ drivers/net/ethernet/freescale/enetc/enetc.c
@@ -5020,6 +5020,15 @@ static const struct enetc_drvdata enetc4_pf_data = {
 	.eth_ops = &enetc4_pf_ethtool_ops,
 };
 
+static const struct enetc_drvdata enetc4_6_pf_data = {
+	.sysclk_freq = ENETC_CLK_400M,
+	.tx_csum = true,
+	.shared_tx_rings = true,
+	.max_frags = ENETC4_MAX_SKB_FRAGS,
+	.pmac_offset = ENETC4_PMAC_OFFSET,
+	.eth_ops = &enetc4_pf_ethtool_ops,
+};
+
 static const struct enetc_drvdata enetc4_ppm_data = {
 	.sysclk_freq = ENETC_CLK_333M,
 	.tx_csum = true,
@@ -5036,6 +5045,14 @@ static const struct enetc_drvdata enetc_vf_data = {
 
 static const struct enetc_drvdata enetc4_vf_data = {
 	.sysclk_freq = ENETC_CLK_333M,
+	.tx_csum = true,
+	.shared_tx_rings = true,
+	.max_frags = ENETC4_MAX_SKB_FRAGS,
+	.eth_ops = &enetc_vf_ethtool_ops,
+};
+
+static const struct enetc_drvdata enetc4_6_vf_data = {
+	.sysclk_freq = ENETC_CLK_400M,
 	.tx_csum = true,
 	.shared_tx_rings = true,
 	.max_frags = ENETC4_MAX_SKB_FRAGS,
@@ -5074,11 +5091,11 @@ static const struct enetc_platform_info enetc_info[] = {
 	},
 	{ .revision = ENETC_REV_4_6,
 	  .dev_id = NXP_ENETC_PF_DEV_ID,
-	  .data = &enetc4_pf_data,
+	  .data = &enetc4_6_pf_data,
 	},
 	{ .revision = ENETC_REV_4_6,
 	  .dev_id = NXP_ENETC_VF_DEV_ID,
-	  .data = &enetc4_pf_data,
+	  .data = &enetc4_6_vf_data,
 	},
 };
 
