f7c31a5b93f0066742385a636e214c5c499dcbe1
Zhipeng Wang <zhipeng.wang_1@nxp.com>
2026-01-08 11:52:23 +0900

    MA-23943 PCI: Drop platform-specific AER/PME interrupt support for DWC
    44acffe2b78 Revert "PCI/portdrv: Use get_service_irqs() callback to get PME/AER irqs"
    224ae4128bf Revert "PCI: dwc: Fetch dedicated AER/PME interrupters"
    
    This patch is not upstreamed and breaks KMI. While it affects AER/PME
    functionality on i.MX9 devices, normal PCIe operations are not impacted.
    Reverting to maintain KMI stability.
    
    Change-Id: Ie0418f720c5a3e90c094e2d2f60e7e87394b13a8

--- include/linux/pci.h
+++ include/linux/pci.h
@@ -606,7 +606,6 @@ struct pci_host_bridge {
 	u8 (*swizzle_irq)(struct pci_dev *, u8 *); /* Platform IRQ swizzler */
 	int (*map_irq)(const struct pci_dev *, u8, u8);
 	void (*release_fn)(struct pci_host_bridge *);
-	int (*get_service_irqs)(struct pci_host_bridge *bridge, int *irqs, int mask);
 	int (*enable_device)(struct pci_host_bridge *bridge, struct pci_dev *dev);
 	void (*disable_device)(struct pci_host_bridge *bridge, struct pci_dev *dev);
 	void		*release_data;
