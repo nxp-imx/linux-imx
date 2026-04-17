a468b841e2d94592b54aee447697ea1e7ebb4486
Zhipeng Wang <zhipeng.wang_1@nxp.com>
2026-04-17 17:00:00 +0900

    MA-24743 pmdomain: imx: imx8m-blk-ctrl: fix genpd leak on probe defer
    When the trusty device check fails and probe returns -EPROBE_DEFER,
    the bus_power_dev was not being detached. This causes a genpd virtual
    device (e.g., genpd:0:38330000.blk-ctrl) to remain registered in sysfs.
    
    On the next deferred probe attempt, dev_pm_domain_attach_by_name()
    tries to create the same genpd device again, resulting in:
    
      sysfs: cannot create duplicate filename '/devices/genpd:0:38330000.blk-ctrl'
      imx8m-blk-ctrl 38330000.blk-ctrl: error -EEXIST: failed to attach power domain "bus"
    
    Fix this by calling dev_pm_domain_detach() for bus_power_dev before
    returning -EPROBE_DEFER in the trusty check path.
    
    Change-Id: I2225a995d0418bfd6d1fd354b8bcc49ffdac3211
    Signed-off-by: Zhipeng Wang <zhipeng.wang_1@nxp.com>

--- drivers/pmdomain/imx/imx8m-blk-ctrl.c
+++ drivers/pmdomain/imx/imx8m-blk-ctrl.c
@@ -346,8 +346,10 @@ static int imx8m_blk_ctrl_probe(struct platform_device *pdev)
 	/* if the device contains trusty node, use the smcc to control the register */
 	if (of_find_property(np, "trusty", NULL)) {
 		bc->trusty_dev = bus_find_device_by_name(&platform_bus_type, NULL, "trusty-core");
-		if (!bc->trusty_dev || !bc->trusty_dev->driver || !dev_get_drvdata(bc->trusty_dev))
+		if (!bc->trusty_dev || !bc->trusty_dev->driver || !dev_get_drvdata(bc->trusty_dev)) {
+			dev_pm_domain_detach(bc->bus_power_dev, true);
 			return -EPROBE_DEFER;
+		}
 
 		if (!trusty_fast_call32(bc->trusty_dev, SMC_HANTRO_PROBE, 0, 0, 0)) {
 			dev_info(dev, "BLK CTRL use trusty mode\n");
