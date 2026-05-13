65ca48cf335b838c91c43e3ee0781a3fdd41a043
Joy Zou <joy.zou@nxp.com>
2026-05-13 16:12:06 +0800

    LF-18020-2 soc: imx: Support i.MX937
    Add i.MX937 machine compatible to allow soc devices could be created.
    
    Reviewed-by: Ye Li <ye.li@nxp.com>
    Signed-off-by: Joy Zou <joy.zou@nxp.com>

--- drivers/soc/imx/soc-imx9.c
+++ drivers/soc/imx/soc-imx9.c
@@ -91,6 +91,7 @@ static __maybe_unused const struct of_device_id imx9_soc_match[] = {
 	{ .compatible = "fsl,imx94", },
 	{ .compatible = "fsl,imx95", },
 	{ .compatible = "fsl,imx952", },
+	{ .compatible = "fsl,imx937", },
 	{ }
 };
 
