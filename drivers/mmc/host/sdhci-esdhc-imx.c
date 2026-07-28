74bdcdbf3b5540114bb72ddf3ae329dd78e65dce
Luke Wang <ziniu.wang_1@nxp.com>
2026-07-28 14:34:28 +0800

    mmc: sdhci-esdhc-imx: make non-fatal errors non-blocking in suspend
    Make pinctrl_pm_select_sleep_state() and mmc_gpio_set_cd_wake() failures
    non-fatal in the suspend path. These failures only mean slightly higher
    power consumption or missing CD wakeup capability, but should not block
    system suspend.
    
    Also change the function to always return 0 on the success path instead
    of propagating non-fatal warning return values.
    
    Fixes: 676a83855614 ("mmc: host: sdhci-esdhc-imx: refactor the system PM logic")
    Acked-by: Adrian Hunter <adrian.hunter@intel.com>
    Signed-off-by: Luke Wang <ziniu.wang_1@nxp.com>
    Cc: stable@vger.kernel.org
    Signed-off-by: Ulf Hansson <ulfh@kernel.org>
    (cherry picked from commit 6aa00a43bbd3d994558a55586351757cebbff236)

--- drivers/mmc/host/sdhci-esdhc-imx.c
+++ drivers/mmc/host/sdhci-esdhc-imx.c
@@ -2142,8 +2142,7 @@ static int sdhci_esdhc_suspend(struct device *dev)
 	disable_irq(host->irq);
 
 	if (device_may_wakeup(dev)) {
-		ret = sdhci_enable_irq_wakeups(host);
-		if (!ret)
+		if (!sdhci_enable_irq_wakeups(host))
 			dev_warn(dev, "Failed to enable irq wakeup\n");
 	} else {
 		/*
@@ -2154,13 +2153,14 @@ static int sdhci_esdhc_suspend(struct device *dev)
 		 * other function like GPIO function to save power in PM,
 		 * which finally block the SDIO wakeup function.
 		 */
-		ret = pinctrl_pm_select_sleep_state(dev);
-		if (ret)
-			return ret;
+		if (pinctrl_pm_select_sleep_state(dev))
+			dev_warn(dev, "Failed to select sleep pinctrl state\n");
 	}
 
-	if (imx_data->boarddata.cd_gpio_wakeup)
-		ret = mmc_gpio_set_cd_wake(host->mmc, true);
+	if (imx_data->boarddata.cd_gpio_wakeup) {
+		if (mmc_gpio_set_cd_wake(host->mmc, true))
+			dev_warn(dev, "Failed to enable cd wake\n");
+	}
 
 	/*
 	 * Make sure invoke runtime_suspend to gate off clock.
@@ -2168,7 +2168,7 @@ static int sdhci_esdhc_suspend(struct device *dev)
 	 */
 	pm_runtime_force_suspend(dev);
 
-	return ret;
+	return 0;
 }
 
 static int sdhci_esdhc_resume(struct device *dev)
